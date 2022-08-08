20f9373d2369330ef701b91eb67ea520332acfc2
Ross Wintle <rosswintle@users.noreply.github.com>
2022-08-08 11:15:31 +0100

    Update build dependencies and Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,25 +1,25 @@
 var gulp = require( 'gulp' );
-var sass = require( 'gulp-sass' );
+var gulpSass = require( 'gulp-sass' )(require('sass'));
 var rename = require( 'gulp-rename' );
 var cssmin = require( 'gulp-cssmin' );
 var uglify = require( 'gulp-uglify' );
 var livereload = require( 'gulp-livereload' );
 
-gulp.task( 'sass', function() {
+function sass() {
 	return gulp.src( 'assets/scss/**/*.scss' )
-		.pipe( sass().on( 'error', sass.logError ) )
+		.pipe( gulpSass.sync().on( 'error', gulpSass.logError ) )
 		.pipe( gulp.dest( 'assets/css' ) )
 		.pipe( livereload() );
-} );
+};
 
-gulp.task( 'css', [ 'sass' ], function() {
+function css () {
 	return gulp.src( [ 'assets/css/**/*.css', '!assets/css/**/*.min.css' ] )
 		.pipe( rename( { suffix: '.min' } ) )
 		.pipe( cssmin() )
 		.pipe( gulp.dest( 'assets/css' ) )
-} );
+};
 
-gulp.task( 'js', function() {
+function js () {
 	return gulp.src( [ 'assets/js/**/*.js', '!assets/js/**/*.min.js' ] )
 		.pipe( uglify( {
 			mangle: false
@@ -27,12 +27,16 @@ gulp.task( 'js', function() {
 		.pipe( rename( { suffix: '.min' } ) )
 		.pipe( gulp.dest( 'assets/js' ) )
 		.pipe( livereload() );
-} );
+};
 
-gulp.task( 'default', [ 'css', 'js' ] );
+exports.sass = sass;
+exports.css = css;
+exports.js = js;
 
-gulp.task( 'watch', [ 'css', 'js' ], function() {
-	livereload.listen();
-	gulp.watch( [ 'assets/scss/**/*.scss' ], [ 'css' ] );
-	gulp.watch( [ 'assets/js/**/*.js' ], [ 'js' ] );
-} );
+exports.build = gulp.series( sass, css, js );
+
+exports.watch = gulp.series( sass, css, js, () => {
+        livereload.listen();
+        gulp.watch( [ 'assets/scss/**/*.scss' ], gulp.series( sass, css ) );
+        gulp.watch( [ 'assets/js/**/*.js' ], gulp.series( js ) );
+    } );
